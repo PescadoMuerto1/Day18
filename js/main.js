75ff7f6a1b4632de01fc0c5398dfea9c721c7c69
PescadoMuerto1 <mbl0502727793@egmail.com>
2024-02-13 14:21:35 +0200

    Add ball growth limit

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@
 var gBallSize = 100
 
 function onBallClick(elBall){
+    if(gBallSize === 400) gBallSize = 100
     gBallSize += 50
     elBall.style.width = gBallSize + 'px'
     elBall.style.height = gBallSize + 'px'
